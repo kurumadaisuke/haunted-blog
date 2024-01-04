57c19e7a6c97163d672cb5069729dc135ba44f5f
kurumadaisuke <kuruma0909@gmail.com>
2024-01-04 20:34:23 +0900

    blogコントローラーリセット&別ユーザーのブログ編集、削除できないように

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,6 @@ class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   before_action :set_blog, only: %i[show edit update destroy]
-  before_action :correct_user, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
@@ -16,7 +15,9 @@ class BlogsController < ApplicationController
     @blog = Blog.new
   end
 
-  def edit; end
+  def edit
+    @blog = current_user.blogs.find(params[:id])
+  end
 
   def create
     @blog = current_user.blogs.new(blog_params)
@@ -29,18 +30,18 @@ class BlogsController < ApplicationController
   end
 
   def update
-    if current_user.premium || !params[:blog][:random_eyecatch]
-      if @blog.update(blog_params)
-        redirect_to blog_url(@blog), notice: 'Blog was successfully updated.'
-      else
-        render :edit, status: :unprocessable_entity
-      end
+    @blog = current_user.blogs.find(params[:id])
+
+    if @blog.update(blog_params)
+      redirect_to blog_url(@blog), notice: 'Blog was successfully updated.'
     else
-      redirect_to root_url, alert: 'Regular users cannot set random_eyecatch.'
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
+    @blog = current_user.blogs.find(params[:id])
+
     @blog.destroy!
 
     redirect_to blogs_url, notice: 'Blog was successfully destroyed.', status: :see_other
@@ -49,19 +50,10 @@ class BlogsController < ApplicationController
   private
 
   def set_blog
-    @blog = if current_user.nil?
-              Blog.where(secret: false).find(params[:id])
-            else
-              Blog.find(params[:id]).secret ? current_user.blogs.find(params[:id]) : Blog.find(params[:id])
-            end
+    @blog = Blog.find(params[:id])
   end
 
   def blog_params
     params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
-  end
-
-  def correct_user
-    user = @blog.user
-    raise ActiveRecord::RecordNotFound unless user == current_user
   end
 end
